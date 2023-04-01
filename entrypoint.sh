7c4bba9ee05ee65cd8be9a905ef167cc79b17b22
Matt Shields <matt@shields.tv>
2023-04-01 18:14:04 -0400

    Test logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@ which gunicorn
 cp /app/data/config/* /app/config/
 cp /app/data/images/* /app/static/
 cd /app
+ls -lasF /app
+ls -lasF /app/config
+ls -lasF /app/static
 gunicorn app:app -w 4 --threads 2 -b 0.0.0.0:80
